6f840428cea7630a951766636eb2f27dce29b4e7
LokiGMK <83254061+LokiGMK@users.noreply.github.com>
2022-09-15 10:38:22 -0400

    Delete LED_Blink directory

--- LED_Blink/target/2.3.0/argon/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-c:/Users/nport/OneDrive/Classes/OCN479/LED_Blink/target/2.3.0/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
